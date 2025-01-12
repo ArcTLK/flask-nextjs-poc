53b49d479c2a525aae80442117272769f065222b
Aditya Nair <33800818+ArcTLK@users.noreply.github.com>
2025-01-12 20:43:39 +0530

    refactor: use dedicated routes instead of static redirects

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from flask import Flask, redirect
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='public', static_url_path='/frontend')
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Root path"
 
 @app.route("/frontend")
-def frontend_redirect():
-    return redirect("/frontend/index.html")
+def serve_frontend_root():
+    # TODO: add authentication check here
+
+    return send_from_directory('public', 'index.html')
+
+@app.route("/frontend/<path:path>")
+def serve_frontend_resource_files(path):
+    # TODO: add authentication check here
+    
+    return send_from_directory('public', path)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
